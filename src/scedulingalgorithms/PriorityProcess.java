13f4281693e0c4a1a013b27c0a222cc8496b0a74
Leliso Agegnehu <lelisoagegnehu491@gmail.com>
2021-12-15 11:58:27 +0300

    ayele

--- src/scedulingalgorithms/PriorityProcess.java
+++ src/scedulingalgorithms/PriorityProcess.java
@@ -3,6 +3,9 @@ public class PriorityProcess {
     public int brustTime;
     public int priority;//attributes
     public int finishTime;
+    public int finishTime1;
+    public int finishTime2;
+    public int finishTime3;
 
     public PriorityProcess(int brustTime, int priority) {//constructor
         this.brustTime = brustTime;
